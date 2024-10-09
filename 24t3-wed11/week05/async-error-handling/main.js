cb1c030047c26009be6f2bc8c7c0bb3ad34b1a40
Sofia De Bellis <sofia.debellis23@gmail.com>
2024-10-09 11:56:20 +1100

    tutorial

--- 24t3-wed11/week05/async-error-handling/main.js
+++ 24t3-wed11/week05/async-error-handling/main.js
@@ -1,5 +1,34 @@
 const API_URL = 'https://jsonplaceholder.typicode.com'
 
 function getUserByIdAndUsername(id, username) {
-  return Promise.resolve()
+  return fetch(`${API_URL}/users/${id}`)
+    .then(response => {
+      if (response.status === 404) {
+        return Promise.reject(new Error(`Could not find a user with id ${id}`))
+      }
+      console.log(response);
+      return response.json();
+    })
+    .then(user => {
+      if (user.username !== username) {
+        throw new Error(`The user with id ${id} does not have username '${username}`);
+      }
+      return user;
+    })
 }
+
+const resultElement = document.getElementById('result');
+const body = document.body;
+
+body.classList.add("loading");
+
+getUserByIdAndUsername('1', 'fdfdsfdsafds')
+  .then(user => {
+    resultElement.innerText = JSON.stringify(user);
+  })
+  .catch(err => {
+    alert(err);
+  })
+  .finally(() => {
+    body.classList.replace('loading', 'done');
+  })
